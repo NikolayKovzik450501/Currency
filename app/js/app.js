ba210e4712ff3eccf5c70444dff06a29daa5aa77
Artsiom_Shushkevich <artsiomshushkevich@gmail.com>
2016-12-22 14:55:13 +0300

    add filtering currencies which cannot be converted to belarussian rubles

--- app/js/app.js
+++ app/js/app.js
@@ -1,37 +1,55 @@
 (function() {
-    var currencies;
+    var currencies, convertedValue;
+    var valueForConverting = 0;
 
     $.ajax({
         type: 'GET',
         url: 'http://www.nbrb.by/API/ExRates/Currencies'
     }).done(function(response) {
          currencies = response.map(function(item) {
-             return {
-               id: item.Cur_ID,
+            var dateEnd = Date.parse(item.Cur_DateEnd);
+            
+            if (dateEnd < Date.now()) {
+                return null;
+            } 
+            
+            return {
+               code: item.Cur_Code,
                name: item.Cur_Name_Eng,
                abbreviation: item.Cur_Abbreviation   
-             };
+            };
+         }).filter(function(item) {
+             return item !== null;
          });
 
-        initSelect('#from-select');
-        initSelect('#to-select');
+        initSelect();
     });
 
-    $('#amount-for-converting').on('change', function(event) {
-        var amount = event.target.value;
-
+    $('#value-for-converting-input').on('change', function(event) {
+        valueForConverting = +event.target.value;
     });
 
-    function initSelect(selectId) {
-        var $select = $(selectId);
+    $('#convert-btn').on('click', function() {
+        var ratesUrl = 'http://www.nbrb.by/API/ExRates/Rates/' + $('#to-select').val() + '?ParamMode=1';
+
+        $.ajax({
+            type: 'GET',
+            url: ratesUrl
+        }).done(function(response) {
+            convertedValue = +response.Cur_OfficialRate * valueForConverting;
+
+            $('#result').html('result: ' + convertedValue);
+        });
+    });
+
+    function initSelect() {
+        var $select = $('#to-select');
         var selectOptions = '';
 
         for (var i = 0; i < currencies.length; i++) {
-            selectOptions += '<option value="' + currencies[i.id] + '">' + currencies[i].name + '(' + currencies[i].abbreviation + ')' + '</option>';
+            selectOptions += '<option value="' + currencies[i].code + '">' + currencies[i].name + '(' + currencies[i].abbreviation + ')' + '</option>';
         }
 
         $select.append(selectOptions);
     }
-
-
 })();
